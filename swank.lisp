b39591c7a3ae67f50003869ad5b3b1fed2d94b60
Moskvitin Andrey <archimag@gmail.com>
2010-05-12 12:48:30 +0400

    added :slime-archimag to *features*

--- swank.lisp
+++ swank.lisp
@@ -3984,5 +3984,6 @@ Collisions are caused because package information is ignored."
   (run-hook *after-init-hook*))
 
 (pushnew :swank *features*)
+(pushnew :swank-archimag *features*)
 
 ;;; swank.lisp ends here
